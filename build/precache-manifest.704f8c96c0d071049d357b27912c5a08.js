f21ebf9201e950c5c3b25e091da8c70ddc5841e5
AMOTECHX <94161414+AMOTECHX@users.noreply.github.com>
2022-02-25 12:34:08 +0300

    upload files
    first commit

---
+++ build/precache-manifest.704f8c96c0d071049d357b27912c5a08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecae3e5e44b979e5257b166b41637690",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e773981835f73cc158ad",
+    "url": "/static/js/2.0d489e6f.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/static/js/2.0d489e6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "071405ccdc96fbd3ef0d",
+    "url": "/static/js/main.cc21b28c.chunk.js"
+  },
+  {
+    "revision": "ed8ed874e871633992a1",
+    "url": "/static/js/runtime-main.a418149b.js"
+  },
+  {
+    "revision": "422fec1079cce450353e924288f8313b",
+    "url": "/static/media/commerce.422fec10.png"
+  }
+]);
